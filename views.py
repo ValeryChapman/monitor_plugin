e2b8bf859bbadfc57270bffa592ff797db0a9434
TheProgersTeam <68988841+ValeryChapman@users.noreply.github.com>
2021-08-29 18:42:08 +0300

    [ADD] add monitor_handle_exception

--- views.py
+++ views.py
@@ -22,3 +22,19 @@ class MonitorAPIView(config.MONITOR_BASE_API_VIEW):
 
         # return base ApiView
         return super().handle_exception(exc)
+    
+
+def monitor_handle_exception(exc: Exception, context: dict):
+
+    # get error class
+    exception_class = exc.__class__.__name__
+    
+    # check the error in the list of ignored errors
+    if exception_class in config.MONITOR_IGNORE_LIST:
+        return
+    
+    # get error text
+    exception_text = str(exc)
+
+    # send error to monitor
+    MonitorRequest().Error(exception_class, exception_text)
